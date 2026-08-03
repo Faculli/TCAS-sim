cf6d781c79ee65fae0b1c5b1d9c337db0ee516c4
Faculli <gafaciulli@gmail.com>
2026-08-03 18:57:48 -0300

    feat: base entities added

---
+++ entities/aircraft.py
@@ -0,0 +1,13 @@
+class Aircraft:
+    def __init__(self, callsign, pos, speed, heading, new_altitude):
+        self.callsign = callsign
+        self.pos = pos
+        self.speed = speed
+        self.heading = heading
+        self.newAltitude = new_altitude
+
+    #def update_pos(self, delta_time):
+
+    #def change_heading(self, new_heading):
+
+    #def change_altitude(self, new_altitude):
---
+++ entities/radar.py
@@ -0,0 +1,10 @@
+class Radar:
+    def __init__(self, scan_range, hor_alert_dist, vert_alert_dist):
+        self.scan_range = scan_range
+        self.hor_alert_dist = hor_alert_dist
+        self.vert_alert_dist = vert_alert_dist
+
+    #def scan_aerial_space(self, aircraft_list):
+
+    #def calc_distance(airc1, airc2):
+    
