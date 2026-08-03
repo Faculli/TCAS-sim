1d93ed0234db1c1b0f856cd1b58b74420d8445fb
Faculli <gafaciulli@gmail.com>
2026-08-03 19:12:34 -0300

    feat: init adition + base services layer

---
+++ services/atc_control.py
@@ -0,0 +1,7 @@
+class ATCControl:
+    def __init__(self, radar):
+        self.radar = radar
+
+    #def process_alerts(self):
+
+    #def reroute_suggestion(self, airc1, airc2):
---
+++ services/simulator.py
@@ -0,0 +1,11 @@
+class Simulator:
+    def __init__(self, elapsed_time, aircraft_list, radar):
+        self.elapsed_time = elapsed_time
+        self.aircraft_list = aircraft_list
+        self.radar = radar
+
+    #def add_aircraft(self):
+
+    #def run_cycle(delta_time):
+
+    #def obtain_current_status():
